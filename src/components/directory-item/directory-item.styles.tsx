d2f1e1bc9845d5c2044c0f77c3771d80bf4c51f3
Artem Valkov <valkov.artyom.fs@gmail.com>
2022-12-13 14:35:02 +0200

    fix

--- src/components/directory-item/directory-item.styles.tsx
+++ src/components/directory-item/directory-item.styles.tsx
@@ -40,7 +40,7 @@ export const Body = styled.div`
 
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
-  height: 240px;
+  height: 450px;
   flex: 1 1 auto;
   display: flex;
   align-items: center;
